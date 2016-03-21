fd1de1c3b96103f3f0a8dd28080f2c4ba87e2f1a
Roma <anton777kratiyk@gmail.com>
2016-03-21 18:14:19 +0200

    NINJA CODE

---
+++ ConsoleApplication2/Class1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+   
+    public static class Randomizer
+    {
+
+        public static void randomVal(ref int[] arr)
+        {
+            Random r = new Random();
+            int num = r.Next(0, 13);
+           
+            if (num < arr.Length)
+            {
+                arr[num] = 0;                
+            }
+         
+        }
+        public static void Randomize(ref int[] arr, int size)
+        {
+            try
+            {
+                Exception ecxp = new Exception("Not the right size");
+                if (arr.Length + 1 != size)
+                    throw (ecxp);
+            }
+            catch
+            {
+                Console.WriteLine("You have to put rigth number!! you input number has been changed to " + (arr.Length + 1));
+                size = arr.Length + 1;
+            }
+            finally
+            {
+                int count = 0;
+                for (var i = 0; i < arr.Length;)
+                {
+                    Random rnd = new Random();
+                    arr[i] = rnd.Next(1, size);
+                    for (var j = 0; j < i; ++j)
+                    {
+                        if (arr[j] == arr[i])
+                            ++count;
+                    }
+                    if (count == 0)
+                        ++i;
+                    count = 0;
+                }
+            }
+
+
+        }
+    }
+
+  
+
+    public static class Show<T>
+    {
+        public static bool Display(T[]arr)
+        {
+            if(arr.Length == 0)
+            {
+                Console.WriteLine("Nothing to show!!!");
+                return false;
+            }
+            for( var i =0; i < arr.Length; ++i)
+                Console.Write(arr[i] + " ");
+            return true;
+
+        }
+    }
+    public static class Compare<T>
+    {
+
+        public static void Sum(int size, ref int sum)
+        {
+
+            if (size == 0)
+                return;
+            else
+            {
+                sum += size;
+                --size;
+                Sum(size, ref sum);
+            }
+
+        }
+        public static void SumArr(int[] arr, ref int size, ref int sum)
+        {
+
+            if (size < 0)
+                return;
+            else
+            {
+                sum += arr[size];
+                --size;
+                SumArr(arr, ref size, ref sum);
+            }
+        }
+
+
+        public static bool Comp(int[] arr)
+        {
+            int sum = 0;
+            Compare<T>.Sum(arr.Length, ref sum);
+            int t = arr.Length - 1, k = 0;
+            Compare<T>.SumArr(arr, ref t, ref k);
+            if (sum == k)
+                return true;
+            return false;
+        }
+
+        public static T[] Clone(T[]arr)
+        {
+            T[] arr1 = new T[arr.Length];
+            for(int key = 0; key < arr.Length; ++key)
+            {
+                arr1[key] = arr[key];
+            }
+
+            return arr1;
+        }
+    }
+
+       
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] arr = new int[10];
+            Randomizer.Randomize(ref arr, 11);           
+            Show<int>.Display(arr);
+
+            int[] array = Compare<int>.Clone(arr);
+
+            Randomizer.randomVal(ref array);
+
+            Console.WriteLine();
+
+            Show<int>.Display(array);
+
+
+
+            
+            try
+            {
+                Exception exc = new Exception();
+                if (Compare<int>.Comp(array))
+                    throw exc;
+            }
+            catch
+            {
+                Console.WriteLine("Identic");
+                System.Environment.Exit(1);
+            }
+
+
+
+            int pos = Array.IndexOf(array, 0);
+            Console.WriteLine();
+            Console.WriteLine( "Differs in " + pos);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
